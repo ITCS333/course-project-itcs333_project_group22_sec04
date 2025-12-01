770424c156e1db59264309cb83b5c63744d8a71f
Abdulrahman Ahmed Mashrah <johanliebertemv@gmail.com>
2025-12-01 20:56:01 +0300

    add admin dashboard

---
+++ admin_dashboard.php
@@ -0,0 +1,24 @@
+<?php
+require 'config.php';
+require 'functions.php';
+
+requireAdmin();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Dashboard - ITCS333</title>
+</head>
+<body>
+    <h1>Admin Dashboard</h1>
+    <p>Welcome, <?= htmlspecialchars($_SESSION['name']) ?> (Admin)</p>
+
+    <ul>
+        <li><a href="admin_students.php">Manage Students</a></li>
+        <li><a href="admin_change_password.php">Change My Password</a></li>
+        <li><a href="index.php">Go to Homepage</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</body>
+</html>
