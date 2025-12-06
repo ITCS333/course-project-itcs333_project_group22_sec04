e6fff287f1c7b711aabb543c667cd0620882c594
AhammedIsmail2004 <profacc01478@gmail.com>
2025-12-06 22:12:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,13 @@
 
     <?php if (isLoggedIn()): ?>
         <p>Hello, <?= htmlspecialchars($_SESSION['name']) ?>!</p>
-        <?php if (isAdmin()): ?>
-            <a href="admin_dashboard.php">Admin Dashboard</a>
-        <?php endif; ?>
-        <a href="logout.php">Logout</a>
+        <p>
+            <a href="view_resources.php">View Course Resources</a>
+            <?php if (isAdmin()): ?>
+                | <a href="admin_dashboard.php">Admin Dashboard</a>
+            <?php endif; ?>
+            | <a href="logout.php">Logout</a>
+        </p>
     <?php else: ?>
         <a href="login.php">Login</a>
     <?php endif; ?>
